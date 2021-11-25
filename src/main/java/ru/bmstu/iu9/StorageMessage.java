abb4809c3612e8d8079af87e5add6439cffa0a89
DionisSerbin <reaganusm@gmail.com>
2021-11-25 13:24:25 +0300

    Scripted auto-commit on change (2021-11-25 13:24:25) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/StorageMessage.java
+++ src/main/java/ru/bmstu/iu9/StorageMessage.java
@@ -1,4 +1,13 @@
 package ru.bmstu.iu9;
 
 public class StorageMessage {
+    private String url;
+
+    public Message(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
 }
