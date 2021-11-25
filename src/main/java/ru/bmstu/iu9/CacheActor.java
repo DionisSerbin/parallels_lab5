fa1725c988d9c789594e303ff48e6e2a6a31caa8
DionisSerbin <reaganusm@gmail.com>
2021-11-25 13:17:07 +0300

    Scripted auto-commit on change (2021-11-25 13:17:06) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/CacheActor.java
+++ src/main/java/ru/bmstu/iu9/CacheActor.java
@@ -1,4 +1,11 @@
 package ru.bmstu.iu9;
 
-public class CacheActor {
+import akka.actor.AbstractActor;
+
+public class CacheActor extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
