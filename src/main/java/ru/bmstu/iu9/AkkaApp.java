e5ca3a0433f53ab56afdc1740fed0cf02f35e005
DionisSerbin <reaganusm@gmail.com>
2021-11-25 13:29:30 +0300

    Scripted auto-commit on change (2021-11-25 13:29:30) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/AkkaApp.java
+++ src/main/java/ru/bmstu/iu9/AkkaApp.java
@@ -3,6 +3,8 @@ package ru.bmstu.iu9;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 import java.io.IOException;
 
@@ -11,7 +13,9 @@ public class AkkaApp {
     public static void main(String[] args) throws IOException {
         System.out.println("Server is starting");
         ActorSystem system = ActorSystem.create("routes");
-        ActorRef actorRef = system.actorOf(Props.create())
+        ActorRef actorRef = system.actorOf(Props.create(CacheActor.class));
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
     }
 
 }
