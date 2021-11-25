692863c6dab882e62ca342d042a4b364902aa744
DionisSerbin <reaganusm@gmail.com>
2021-11-25 13:19:34 +0300

    Scripted auto-commit on change (2021-11-25 13:19:34) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/Message.java
+++ src/main/java/ru/bmstu/iu9/Message.java
@@ -4,6 +4,10 @@ public class Message {
     private String url;
 
     public Message(String url){
-        th
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return this.url;
     }
 }
