30decbb53a4d036e28f2b95ea89a602dee5c26aa
DionisSerbin <reaganusm@gmail.com>
2021-11-25 13:22:08 +0300

    Scripted auto-commit on change (2021-11-25 13:22:07) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/CacheActor.java
+++ src/main/java/ru/bmstu/iu9/CacheActor.java
@@ -3,14 +3,20 @@ package ru.bmstu.iu9;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
+
 public class CacheActor extends AbstractActor {
+    private final HashMap<String, Integer> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.
                 create().
                 match(
-
+                        Message.class,
+                        m -> {
+                            getSender().tell();
+                        }
                 )
     }
 }
